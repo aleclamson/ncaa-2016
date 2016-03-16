7a9636e7253ef823fd6b10ecd0799b1a117b94db
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-03-16 10:08:50 -0600

    has the readfile class

--- src/readfile.java
+++ src/readfile.java
@@ -1,22 +1,67 @@
 import java.io.File;
-import java.util.Scanner;
+import java.util.*;
+
 public class ReadFile {
+	
 	public static void main(String[] args) {
+		ArrayList<String> data = new ArrayList<String>();
+		ArrayList<Team> teams = new ArrayList<Team>();
 		
 		try {
 			Scanner input = new Scanner(System.in);
-			System.out.print("ncaa_data_2016.csv");
-			File file = new File(input.nextLine());
 			
-			input = new Scanner(file);
+			File file = new File("ncaa_data_2016.csv");  
 			
-			while (input.hasNextLine()) {
+			Scanner scanner = input = new Scanner(file);
+			
+			while(input.hasNextLine()) {
 				String line = input.nextLine();
-				System.out.println(line);
-			}
+				data.add(line);
+			 }
 			
+			for (int i = 1; i < 65; i++ ) {
+				String numbers = (data.get(i));
+				String[] septeam = numbers.split(",");
+				for (int j = 0; j < 1; j++) {
+					Team team = new Team(septeam[0],Double.parseDouble(septeam[1]), septeam[2], Integer.parseInt(septeam[3]));
+					teams.add(team);
+					
+				  }
+			   }
+			 input.close();
+			
+			}  catch (Exception ex) {
+			ex.printStackTrace();
+			  }
+ 
+		getFinal(teams);
+     
+	}
+	public static Team getWinner(Team team1, Team team2) {
+		Random r =  new Random();
+		double random = r.nextDouble();
+		if ( random <= probability(team1, team2)) {
+			return team1;
+		}
+		else {
+			return team2;
 		}
 	}
-
+	
+	public static double probability(Team team1, Team team2) {
+		double probability = 0;
+		probability = (team1.getExpected() - (team1.getExpected() * team2.getExpected()))/ (team1.getExpected() + team2.getExpected() - 2* team1.getExpected() * team2.getExpected());
+		//equation for the probabity
+		return probability;
+	}
+	
+	public static ArrayList<Team> sortTeam(ArrayList<Team> numbers, String region) {
+		ArrayList<Team> newTeam = new ArrayList<Team>();
+		for (Team x : numbers) {
+			if (x.getRegion().equals(region)) {
+				newTeam.add(x);
+			}
+		}
+	 return newTeam;
+	}
 }
-
